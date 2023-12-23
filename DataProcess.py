7abc6e0aa7267cf4f200e5d52bd41c272ea5f320
xmddd <1720648358@qq.com>
2023-12-23 15:18:50 +0800

    finish wjy part

--- DataProcess.py
+++ DataProcess.py
@@ -1,37 +1,102 @@
 from pandas import read_csv
 import torch
 from torch import nn
+import torch.nn.functional as F
+from torch.utils.data import Dataset
 from tqdm import tqdm
 
-TrainSet = read_csv("./data/train.csv")
+
+truncation = 20  # The truncation length
+
+
+class EBDataset(Dataset):  # Electronic Business Dataset
+    def __init__(self):
+        self.TrainSet = read_csv(
+            "./data/train.csv",
+            usecols=["event_type", "product_id", "user_id"],
+            dtype={"event_type": str, "product_id": str, "user_id": str},
+        )
+        self.EventDict = {
+            "view": 0,
+            "cart": 1,
+            "remove_from_cart": 2,
+            "purchase": 3,
+            "padding": 4,
+        }  # The id of each event type
+        self.users = -1  # The number of users in TrainSet after truncating
+        self.RecordList = {"Prod": [], "Event": []}  # The truncated records of user
+        self.ProdList = ["000"]  # The product list mapped ith product to its id
+        self.ProdDict = {
+            "000": 0
+        }  # The product dictionary mapped a product's id to i, the '000' represents the padding product
+
+        # Encode products, construct ProdList and ProdDict
+        for index, row in tqdm(
+            self.TrainSet.iterrows(), total=len(self.TrainSet), desc="Encode products"
+        ):
+            if not row["product_id"] in self.ProdList:
+                self.ProdDict[row["product_id"]] = len(self.ProdList)
+                self.ProdList.append(row["product_id"])
+        
+        # Organize records, do truncation and padding
+        self.TrainSet = self.TrainSet.groupby("user_id")
+        for user_id, PartSet in tqdm(self.TrainSet, desc="Organize records"):
+            i = 0
+            # Truncation
+            while len(PartSet) > i + truncation:
+                self.users += 1  # Create a new user
+                i += truncation
+
+                # Extract each product's index in its records
+                ProdList = PartSet.iloc[i : i + truncation]["product_id"].tolist()
+                ProdIndexList = [self.ProdDict[product_id] for product_id in ProdList]
+                self.RecordList["Prod"].append(ProdIndexList)
+
+                # Extract each event type in its records
+                EventList = PartSet.iloc[i : i + truncation]["event_type"].tolist()
+                EventIndexList = [
+                    self.EventDict[event_type] for event_type in EventList
+                ]
+                self.RecordList["Event"].append(EventIndexList)
+                print(ProdList)
+
+            self.users += 1
+            # Padding product
+            ProdList = PartSet.iloc[i : len(PartSet)]["product_id"].tolist()
+            for i in range(truncation - len(ProdList)):
+                ProdList.append("000")
+            ProdIndexList = [self.ProdDict[product_id] for product_id in ProdList]
+            self.RecordList["Prod"].append(ProdIndexList)
+
+            # Padding event
+            EventList = PartSet.iloc[i : len(PartSet)]["event_type"].tolist()
+            for i in range(truncation - len(EventList)):
+                EventList.append(4)
+            EventIndexList = [self.EventDict[event_type] for event_type in EventList]
+            self.RecordList["Event"].append(EventIndexList)
+
+    def __len__(self):
+        return self.users
+
+    def __getitem__(self, idx):
+        Prods = self.RecordList["Prod"][idx]
+        Events = self.RecordList["Event"][idx]
+        return Prods, Events
+
+
 # TestSet = read_csv("./data/test.csv")
-
-user_id = 0
-handled = 0  # The number of record which have been embedded
-truncation = 20
-
 # Embedding
-# ProductList = [] # The product list mapped ith product to its id
-# ProdDict = {} #The product dictionary mapped a product's id to i
-# for index,row in TrainSet.iterrows():
-#     if not row['product_id'] in ProductList:
-#         ProdDict[row['product_id']] = len(ProductList)
-#         ProductList.append(row['product_id'])
 
-# print(len(ProductList))
+# TotalProducts = len(ProdList)  # 32734
+# embedding = nn.Embedding(TotalProducts + 3, 508)
 
-# embedding = nn.Embedding(len(ProductList), 508)
-maxlen = 0
-maxid=0
-print(len(TrainSet))
-with tqdm(total=len(TrainSet)) as pbar:
-    while handled < len(TrainSet):
-        user_id += 1
-        PartSet = TrainSet[TrainSet["user_id"] == user_id]
-        SequenceLen = len(PartSet)
-        handled += SequenceLen
-        if SequenceLen>maxlen:
-            maxlen = SequenceLen
-            maxid = user_id
-        pbar.update(handled)
-print(maxlen, maxid)
+# Event = torch.tensor(Event)
+# EmbeddingValues = torch.cat((embedding(ProdIndex), F.one_hot(Event)), dim=1)
+# print("EmbeddingValues' size:", EmbeddingValues.size())
+# # TrainSet.insert(loc=len(TrainSet.columns),column='embedding',value = EmbeddingValues)
+
+# # print(len(TrainSet))
+
+# TrainSet = TrainSet.groupby("user_id")
+# for user_id, PartSet in tqdm(TrainSet):
+#     pass
